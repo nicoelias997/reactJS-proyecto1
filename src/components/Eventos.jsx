135114aaafe5d274cd83d82b80e495b78d5ff055
nicoelias997 <nicoelias997@gmail.com>
2022-08-28 20:48:17 +0200

    Agregamos un estado para modificar un texto, usamos useState que dentro llevara nuestro texto principal, y setTexto(aqui llevara la modificaicon que queremos), vemos la sintax de [texto, setTexto]

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -1,13 +1,19 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Eventos = () => {
+
+    const [texto, setTexto] = useState("Texto desde estado")
+
+
+
     const eventoClick = () => {
         console.log("Me diste un click")
+        setTexto("Cambiando el texto")
     }
   return (
         <>
         <hr />
-        <h2>Eventos</h2>
+        <h2> {texto} </h2>
         <button onClick={ () => eventoClick() }>click</button>
         </>
   )
