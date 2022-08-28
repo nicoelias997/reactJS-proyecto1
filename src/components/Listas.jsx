94b013bd2d9eb87ee486b309903a6424a6db615b
nicoelias997 <nicoelias997@gmail.com>
2022-08-28 21:18:53 +0200

    Siempre que quieramos agregar algo, debemos tomar una copia con ...lista, por el evento de propagacion. Ademas nos saldra un error si queremos agregar ese elemento varias veces porque su key no es unica, debemos cambiar nuevamente la key a index, asi cada valor que se agregue, tenga una key unica

--- src/components/Listas.jsx
+++ src/components/Listas.jsx
@@ -8,17 +8,25 @@ const Listas = () => {
         {id: 3, texto: "Tarea 3"}
     ]
 
-    const [lista, setLista] = useState(estadoInicial)
+    const [lista, setLista] = useState(estadoInicial);
 
+    const agregarElementos = () =>{
+        console.log("Me diste click")
+        setLista([
+            ...lista,
+            {id:4, texto: "Tarea 4"}        
+        ])
+    }
 
   return (
     <div>
        <h2>Listas</h2>
         {
             lista.map( (item, index) => (
-                <h4 key={item.id}>{item.texto}</h4>
+                <h4 key={index}>{item.texto}</h4>
             ))
         }
+        <button onClick={() => agregarElementos()}>Agregar</button>
     </div>
   )
 }
