b6e4761061e7fdda5749ae372d00f2031669bb3c
nicoelias997 <nicoelias997@gmail.com>
2022-08-28 19:34:46 +0200

    class = className pero usamos el 2do en react, y como no usamos css ni logos, borramos los import y dejamos unicamente lo de react. Tambien borramos la className ya que utilizaremos clases propias

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
+    <div> 
      <h1>Hola mundo React</h1>
     </div>
   );
